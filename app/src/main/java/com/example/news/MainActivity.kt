7c289a6079c7f1edf1989a34018e6864888061e7
Beh <behnamkhani@hotmail.com>
2023-10-19 04:36:05 -0700

    Add lottie animation and config data binding for the activity_main.xml

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // The purpose of this code is to handle the nullable nature of the binding object.
+    // Since the binding is typically set in the onCreate() method of the activity, it could be null until that point.
+    private var _binding:ActivityMainBinding? = null
+
+    // A read-only property binding of type ActivityMainBinding
+    private val binding:ActivityMainBinding
+    get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
